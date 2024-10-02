7ba5049a2941db83756c9d1d8e6f867bea0eb678
polhidalgo <polhidalgolle7@gmail.com>
2024-10-02 18:16:38 +0200

    Delete Activitat2 directory

--- Activitat2/act12.py
+++
@@ -1,4 +0,0 @@
-numero = int(input("Introdueix numero: "))
-
-suma = sum(range(1, numero + 1))
-print(suma)
--- Activitat2/act6.py
+++
@@ -1,8 +0,0 @@
-
-numero = 1
-while numero <= 100:
-    print(numero)
-    numero += 1
-
-for numero in range(1, 101):
-    print(numero)
